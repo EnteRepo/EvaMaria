525f6acd86188609963f2eb5cfa5eb9f52256f8f
༺✦҈͜͡➳ιgиιтє∂ мιи∂ѕ࿐༻ <91360737+OO7ROBot@users.noreply.github.com>
2021-12-11 10:35:05 +0530

    Minor change

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -460,7 +460,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.HELP_TXT.format(query.from_user.mention),
+            text=script.HELP_TXT,
             reply_markup=reply_markup,
             parse_mode='html'
         )
